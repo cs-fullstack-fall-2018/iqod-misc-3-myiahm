667525e79e14a7114143b8e5607bc44bda4edb5b
Christopher Pace <Christopher_pace@ymail.com>
2018-11-10 13:00:46 -0600

    check in

--- answer.py
+++ answer.py
@@ -1,10 +1,9 @@
 def number(input):
-    for i in range(input):
+    for i in range(input+1):
         s = ""
 
         for x in range(i):
             s+= str(i)
-            print(i)
 
-
+        print(s)
 number(4)
